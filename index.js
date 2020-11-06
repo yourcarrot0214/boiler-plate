a7c984b189c6c0147786e97a9ced81fb389c548a
yourcarrot0214 <stylenbs@gmail.com>
2020-11-06 19:29:01 +0900

    body-parser & postman을 이용한 회원가입기능

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
@@ -23,6 +28,15 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello node & express");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
